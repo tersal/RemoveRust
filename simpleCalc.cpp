52ac33ac353cb2559680ee56776a44cb7a3cf807
Moises Terrones <moises.tersal@gmail.com>
2018-02-17 23:17:57 -0600

    simpleCalc test
    Well this is the code for a really simple calculator in the console, at the moment this thing does nothing more than just display some messages so you should not use this thing, this is far from over and it is mostly used to learn how to use GitHub and remember some coding skill with C++ (even when this thing will look more like C).

---
+++ simpleCalc.cpp
@@ -0,0 +1,51 @@
+/**
+/* This is a really basic calculator program, nothing fancy
+*/
+#include <iostream>
+
+// Just some defines to avoid using magic numbers
+#define EXIT_PROGRAM      0x05  // Used to end the operation of this program
+#define ADD_OPERATION     0x01
+#define MINUS_OPERATION   0x02
+#define MULT_OPERATION    0x03
+#define DIV_OPERATION     0x04
+
+
+int main(int argc, char** argv)
+{
+	int operation;
+	
+	do {
+		std::cout << std::endl << std::endl;
+		std::cout << "List of available operations:" << std::endl;
+		std::cout << "1.- Addition" << std::endl;
+		std::cout << "2.- Substraction" << std::endl;
+		std::cout << "3.- Multiplication" << std::endl;
+		std::cout << "4.- Division" << std::endl;
+		std::cout << "5.- Leave program" << std::endl;
+		std::cout << "Please select an operation from the list: ";
+		std::cin >> operation;
+		
+		switch(operation) {
+			case ADD_OPERATION:
+				// Fill with functionality
+				break;
+			case MINUS_OPERATION:
+				// Fill with functionality
+				break;
+			case MULT_OPERATION:
+				// Fill with functionality
+				break;
+			case DIV_OPERATION:
+				// Fill with functionality
+				break;
+			case EXIT_PROGRAM:
+				std::cout << "Thanks for testing this thing." << std::endl;
+				break;
+			default:
+				std::cout << "I think that I can't do that, yet" << std::endl;
+				break;
+		}
+	}while(operation != EXIT_PROGRAM);	
+	return 0;
+}
